1ed3cb4dae0a9cd17529e3ae2bfcd63ce4dcf654
ruchoa <rodrigo.uchoa@hotmail.com>
2022-08-16 22:54:55 -0300

    autoupdate

---
+++ 42rio/42cursus/push_swap/push_swap.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/16 19:26:01 by ruchoa            #+#    #+#             */
+/*   Updated: 2022/08/16 22:32:22 by ruchoa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdio.h> //printf
+
+# include "../libft/libft.h" //libft
+# include "../ft_printf/ft_printf.h" //ft_printf
+# include "../get_next_line/get_next_line.h" //get_next_line
+
+int		push_swap(void);
+
+#endif
