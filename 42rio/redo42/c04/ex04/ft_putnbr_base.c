45c56b6a48f74080f44f7534f415b38458a12cf1
ruchoa <rodrigo.uchoa@hotmail.com>
2022-09-01 00:18:42 -0300

    autogit

--- 42rio/redo42/c04/ex04/ft_putnbr_base.c
+++ 42rio/redo42/c04/ex04/ft_putnbr_base.c
@@ -6,93 +6,93 @@
 /*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 21:28:11 by ruchoa            #+#    #+#             */
-/*   Updated: 2022/04/05 21:54:03 by ruchoa           ###   ########.fr       */
+/*   Updated: 2022/09/01 00:18:24 by ruchoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
-int	ft_check(char *base);
-
-void	ft_putnbr_base(int nbr, char *base)
+void	ft_putnbr_base(int nb, char	*base)
 {
-	char	nb[32];
+	char	nbr[33];
+	int		len;
 	int		i;
 
-	if (ft_check(base) == 0)
-		return ;
+	len = 0;
+	while (base[len])
+		len++;
 	i = 0;
-	while (nbr != 0)
+	if (nb == 0)
+		write(1, "0", 1);
+	if (nb < 0)
 	{
-		if (nbr > 0)
-		{
-			if (nbr >= ft_check(base))
-				nb[32 - 1 - i] = base[nbr % ft_check(base)];
-			else
-				nb[32 - 1 - i] = base[nbr];
-		}
-		if (nbr < 0)
-		{
-			if (nbr < -ft_check(base))
-				nb[32 - 1 - i] = base[-nbr % ft_check(base)];
-			else
-				nb[32 - 1 - i] = base[-nbr];
-		}
-		nbr /= ft_check(base);
+		write(1, "-", 1);
+		nbr[31 - i] = base[(nb % len) * (-1)];
+		nb = (nb / len) * (-1);
 		i++;
 	}
-	write(1, &nb[32 - i], i);
+	while (nb != 0)
+	{
+		nbr[31 - i] = base[(nb % len)];
+		nb = (nb / len);
+		i++;
+	}
+	nbr[32] = '\0';
+	write(1, &nbr[31 - i], i);
+	printf("%s\n", &nbr[31 - i]);
 }
 
-int	ft_check(char *base)
+/* void	ft_putnbr_base(unsigned long long int nbr, char *base)
 {
-	int	error;
-	int	i;
-	int	j;
+	int	len;
 
-	error = 0;
-	i = 0;
-	while (base[i])
+	len = 0;
+	while (base[len])
+		len++;
+	if (nbr == 0)
 	{
-		j = 0;
-		while (base[j])
-		{
-			if (i != j && base[i] == base[j])
-				error++;
-			j++;
-		}
-		if (base[i] == '+' || base[i] == '-' || base[i] <= 31 || base[i] >= 126)
-			error++;
-		i++;
+		write(1, &base[0], 1);
+		return ;
 	}
-	if (i < 2 || error != 0)
-		return (0);
-	return (i);
-}
+	if (nbr > 0)
+		ft_putnbr_base((nbr /= len), base);
+	if (nbr != 0)
+		write(1, &base[nbr % len], 1);
+} */
+
+int		g_nbr[] = {\
+					16, \
+					};
+
+char	*g_base[] = {\
+					"0123456789abcdefghij", \
+					"0123456789abcdef", \
+					"0123456789", \
+					"01234567", \
+					"poneyvif", \
+					"01", \
+					};
 
 int	main(void)
 {
-	int		nbr;
+	int	size;
+	int	i;
+	int	j;
 
-	nbr = 10;
-	// ft_putnbr_base(nbr, "01");
-	// write(1, "\n", 1);
-	// ft_putnbr_base(nbr, "01234567");
-	// write(1, "\n", 1);
-	// ft_putnbr_base(nbr, "0123456789");
-	// write(1, "\n", 1);
-	ft_putnbr_base(nbr, "0123456789ABCDEF");
-	write(1, "\n", 1);
+	size = (sizeof(g_nbr) / sizeof(*g_nbr));
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (g_base[j])
+		{
+			ft_putnbr_base(g_nbr[i], g_base[j]);
+			write(1, "\n", 1);
+			j++;
+		}
+		write(1, "----------------------------\n", 30);
+		i++;
+	}
 	return (0);
 }
-
-/*
-INT_MAX =  2147483647;
-INT_MIN = -2147483648;
-bin = "01";
-oct = "poneyvif";
-oct = "01234567";
-bin = "0123456789";
-hex = "0123456789ABCDEF";
-hex = "FEDCBA9876543210";
-*/
