2deff2def2fefac6ebd40b06635a1403cf0b3033
RUCHOA <rodrig.uchoa@hotmail.com>
2022-09-05 16:15:43 -0300

    autogit

--- 42rio/42cursus/push_swap/main.c
+++ 42rio/42cursus/push_swap/main.c
@@ -6,11 +6,41 @@
 /*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 21:37:22 by ruchoa            #+#    #+#             */
-/*   Updated: 2022/09/03 22:53:17 by ruchoa           ###   ########.fr       */
+/*   Updated: 2022/09/05 16:11:33 by ruchoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <fcntl.h>
+
+int	main(void)
+{
+	char	**lista;
+	char	*ret;
+	int		*arr;
+	int		count;
+	int		fd;
+	int		i;
+
+	fd = open("./array_de_inteiros.txt", O_RDONLY);
+	ret = get_next_line(fd);
+	lista = ft_split(ret, ',');
+	count = -1;
+	while (lista[count])
+		count++;
+	arr = ft_calloc(count, sizeof(*arr));
+	if (!arr)
+		return(1);
+	i = -1;
+	while (++i < count)
+		arr[i] = ft_atoi(lista[i]);
+	i = -1;
+	while (++i < count)
+		ft_printf("arr[%i] -> %i\n", i, arr[i]);
+	free(arr);
+	close(fd);
+	return (0);
+}
 
 /* int	g_arr[] = {\
 					40, \
@@ -42,7 +72,7 @@ int	main(void)
 	return (0);
 } */
 
-int	main(int argc, char **argv)
+/* int	main(int argc, char **argv)
 {
 	int	*arr;
 	int	i;
@@ -58,4 +88,4 @@ int	main(int argc, char **argv)
 	ft_printf_arr(arr, argc);
 	free(arr);
 	return (0);
-}
+} */
