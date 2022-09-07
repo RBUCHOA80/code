1a886c58c611e80c1a0b82228b8354bf2522ad8b
ruchoa <rodrigo.uchoa@hotmail.com>
2022-09-06 21:59:35 -0300

    autogit

--- 42rio/42cursus/push_swap/main.c
+++ 42rio/42cursus/push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 21:37:22 by ruchoa            #+#    #+#             */
-/*   Updated: 2022/09/06 18:32:39 by ruchoa           ###   ########.fr       */
+/*   Updated: 2022/09/06 21:57:45 by ruchoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,33 @@
 
 int	main(void)
 {
-	char	**lista;
+	char	**list;
 	char	*ret;
 	int		*arr;
 	int		count;
-	int		fd;
 	int		i;
 
-	fd = open("./aux_files/array_de_inteiros.txt", O_RDONLY);
-	ret = get_next_line(fd);
-	lista = ft_split(ret, ',');
+	i = open("./aux_files/array_de_inteiros.txt", O_RDONLY);
+	ret = get_next_line(i);
+	close(i);
+	list = ft_split(ret, ',');
+	free(ret);
 	count = 0;
-	while (lista[count])
+	while (list[count])
 		count++;
 	arr = ft_calloc(count, sizeof(*arr));
 	if (!arr)
-		return(1);
+		return (1);
 	i = -1;
 	while (++i < count)
-		arr[i] = ft_atoi(lista[i]);
-	i = -1;
+		arr[i] = ft_atoi(list[i]);
+	while (i >= 0)
+		free(list[i--]);
+	free(list);
+	ft_merge_sort(arr, 0, count - 1);
 	while (++i < count)
 		ft_printf("arr[%i] -> %i\n", i, arr[i]);
 	free(arr);
-	close(fd);
 	return (0);
 }
 
