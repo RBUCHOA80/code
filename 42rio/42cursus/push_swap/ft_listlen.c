0c6583384b0b2d62e048212c21b4b01089fc96c1
RUCHOA <rodrigo.uchoa@hotmail.com>
2022-09-14 19:28:00 -0300

    autogit

--- 42rio/42cursus/push_swap/ft_listlen.c
+++ 42rio/42cursus/push_swap/ft_listlen.c
@@ -6,7 +6,7 @@
 /*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 18:19:11 by ruchoa            #+#    #+#             */
-/*   Updated: 2022/09/14 19:14:21 by ruchoa           ###   ########.fr       */
+/*   Updated: 2022/09/14 19:27:53 by ruchoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_listlen(t_stack *list)
 	len = 0;
 	while (1)
 	{
-		if (!list[len].next)
+		if (!list[len])
 			break ;
 		len++;
 	}
