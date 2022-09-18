2fab75ecafe438917fe824eb253ad6c186126f6b
ruchoa <rodrigo.uchoa@hotmail.com>
2022-09-18 17:39:24 -0300

    autogit

---
+++ 42rio/42cursus/push_swap2/push_swap.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/16 19:26:01 by ruchoa            #+#    #+#             */
+/*   Updated: 2022/09/18 13:38:19 by ruchoa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h> //malloc / free
+
+# include "../libftprintf/libftprintf.h" //ft_printf
+# include "../libft/libft.h" //libft
+
+/* typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list; */
+
+#endif
