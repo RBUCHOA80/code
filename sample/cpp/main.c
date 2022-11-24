a0f3eb5ea52dc0a80181eb77faf3241afeb81e53
ruchoa <rodrigo.uchoa@hotmail.com>
2022-11-24 19:30:44 -0300

    autogit

---
+++ sample/cpp/main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 19:13:12 by ruchoa            #+#    #+#             */
+/*   Updated: 2022/11/24 19:26:38 by ruchoa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+int	main(void)
+{
+	char	*str;
+	int		nbr;
+
+	nbr = NUMBER;
+	str = "Put your sampletext here.";
+	printf("%s\n", str);
+	printf("%i\n", nbr);
+	printf("%i\n", NUMBER);
+	return (0);
+}
+
+#include "main.h"
