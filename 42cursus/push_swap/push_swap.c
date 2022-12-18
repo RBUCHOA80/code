62f2668e8b8aee6c793ec23188a45c3771be68e1
ruchoa <rodrigo.uchoa@hotmail.com>
2022-12-18 18:40:51 -0300

    autogit

--- 42cursus/push_swap/push_swap.c
+++ 42cursus/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 13:35:50 by ruchoa            #+#    #+#             */
-/*   Updated: 2022/12/18 12:35:28 by ruchoa           ###   ########.fr       */
+/*   Updated: 2022/12/18 18:37:57 by ruchoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,20 +56,20 @@ void	push_swap(t_list **stack_a, t_list **stack_b)
 			}
 			if (ft_lstsize(*stack_a) > 1)
 			{
-				if (ft_atoi((*stack_a)->content) > ft_atoi((*stack_a)->next->content))
+				if (*((int *)(*stack_a)->content) > *((int *)(*stack_a)->next->content))
 					sa(stack_a);
-				else if (ft_atoi((*stack_a)->content) > ft_atoi(ft_lstlast((*stack_a))->content))
+				else if (*((int *)(*stack_a)->content) > *(int *)(ft_lstlast((*stack_a))->content))
 					ra(stack_a);
-				else if (ft_atoi((*stack_a)->content) > ft_atoi(ft_lstlast((*stack_a))->content))
+				else if (*((int *)(*stack_a)->content) > *(int *)(ft_lstlast((*stack_a))->content))
 					rra(stack_a);
 			}
 			if (ft_lstsize(*stack_b) > 1)
 			{
-				if (ft_atoi((*stack_b)->content) < ft_atoi((*stack_b)->next->content))
+				if (*((int *)(*stack_b)->content) < *((int *)(*stack_b)->next->content))
 					sb(stack_b);
-				else if (ft_atoi((*stack_b)->content) < ft_atoi(ft_lstlast((*stack_b))->content))
+				else if (*((int *)(*stack_b)->content) < *(int *)(ft_lstlast((*stack_b))->content))
 					rb(stack_b);
-				else if (ft_atoi((*stack_b)->content) < ft_atoi(ft_lstlast((*stack_b))->content))
+				else if (*((int *)(*stack_b)->content) < *(int *)(ft_lstlast((*stack_b))->content))
 					rrb(stack_b);
 			}
 			i++;
