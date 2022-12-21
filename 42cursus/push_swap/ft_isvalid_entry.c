00aa2aa63b43e39476453bd1a8b38e0687c88b55
ruchoa <rodrigo.uchoa@hotmail.com>
2022-12-21 07:33:43 -0300

    autogit

--- 42cursus/push_swap/ft_isvalid_entry.c
+++ 42cursus/push_swap/ft_isvalid_entry.c
@@ -1,38 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   entry.c                                            :+:      :+:    :+:   */
+/*   ft_isvalid_entry.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 07:31:48 by ruchoa            #+#    #+#             */
-/*   Updated: 2022/12/20 07:36:54 by ruchoa           ###   ########.fr       */
+/*   Updated: 2022/12/21 07:31:08 by ruchoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "./push_swap.h"
 
-#define VALID 0
-#define INVALID 1
-
-int	entry(char **argv)
+int	ft_isvalid_entry(char **argv)
 {
 	int	i;
+	int	j;
 
-	i = 0;
-	while (*argv[1])
+	i = 1;
+	while (argv[i])
 	{
-		/* code */
+		j = 0;
+		while (argv[i][j])
+		{
+			if (!ft_isdigit(argv[i][j]))
+			{
+				ft_printf("argv[%i][%i] = %c\n", i, j, argv[i][j]);
+				return (FALSE);
+			}
+			j++;
+		}
+		i++;
 	}
-	
-	return (VALID);
-	return (INVALID);
+	return (TRUE);
 }
 
 int	main(int argc, char **argv)
 {
 	int	ret;
 
-	ret = entry(argv);
+	(void) argc;
+	ret = ft_isvalid_entry(argv);
+	if (!ret)
+		write(1, "\e[1;31mSYNTAX ERROR\n\e[0m", 24);
+	else if (ret)
+		write(1, "\e[1;32mTRUE ENTRY\n\e[0m", 23);
 	return (0);
 }
