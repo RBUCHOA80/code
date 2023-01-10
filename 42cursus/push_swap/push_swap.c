21edcdaf2114313ee44ee2eda20bee0bcb90e74b
ruchoa <rodrigo.uchoa@hotmail.com>
2023-01-10 05:14:44 -0300

    autogit

--- 42cursus/push_swap/push_swap.c
+++ 42cursus/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 13:35:50 by ruchoa            #+#    #+#             */
-/*   Updated: 2023/01/08 10:45:53 by ruchoa           ###   ########.fr       */
+/*   Updated: 2023/01/10 05:12:12 by ruchoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,28 @@
 
 void	push_swap(t_list **stack_a, t_list **stack_b)
 {
-	while (ft_lstsize(*stack_a))
-		pb(stack_a, stack_b);
-	while (ft_lstsize(*stack_b))
-		pa(stack_b, stack_a);
+	int	len;
+	int	mid;
+
+	if (!(stack_a && stack_b))
+		return ;
+	(void) stack_b;
+	while (ft_lstsize(*stack_a) - 1)
+	{
+		len = ft_lstsize(*stack_a) - 1;
+		mid = len / 2;
+		ft_printf("len = %d\n", len);
+		ft_printf("mid = %d\n", mid);
+		while (len--)
+		{
+			ft_printf("if (%d > %d)\n", *((int *)(*stack_a)->content), mid);
+			if (*((int *)(*stack_a)->content) > mid)
+				pb(stack_a, stack_b);
+			else if (ft_lstsize(*stack_a) > 1)
+				ra(stack_a);
+		}
+	}
+	ft_printf_stacks(*stack_a, *stack_b);
 }
+
+//*((int *)(*stack_a)->content)
