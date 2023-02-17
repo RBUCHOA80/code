1fe1933e5bcf076ba7e786543443638661d28b51
ruchoa <rodrigo.uchoa@hotmail.com>
2023-02-16 21:19:11 -0300

    autogit

--- 42cursus/fract-ol_v5/render.c
+++ 42cursus/fract-ol_v5/render.c
@@ -6,24 +6,12 @@
 /*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 20:42:57 by ruchoa            #+#    #+#             */
-/*   Updated: 2023/02/12 09:18:29 by ruchoa           ###   ########.fr       */
+/*   Updated: 2023/02/16 20:26:32 by ruchoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./fractol.h"
 
-/* set_pixel_color:
-	Add a color to one pixel of the MLX image map.
-	The MLX image is composed of 32 bits per pixel.
-	Color ints are stored from right to left, here, and are in
-	the form of 0xAARRGGBB. 8 bits encode each color component,
-	Alpha (Transparency), Red, Green and Blue.
-	Bit shifting:
-		- BB >> 0   (0x000000BB)
-		- GG >> 8   (0x0000GG00)
-		- RR >> 16  (0x00RR0000)
-		- AA >> 24  (0xAA000000)
-*/
 static void	set_pixel_color(t_fractol *f, int x, int y, int color)
 {
 		f->buf[x * 4 + y * WIDTH * 4] = color;
@@ -53,15 +41,6 @@ static int	calculate_fractal(t_fractol *f, double pr, double pi)
 	return (nb_iter);
 }
 
-/* render:
-*	Iterates through each pixel of the window, translates the pixel's
-*	coordinates into a complex number to be able to calculate if that number
-*	is part of the fractal set or not.
-*	The number of iterations that complex number goes through before being
-*	rejected from the fractal set determines which color is applied to the pixel.
-*	Once all pixels have been assessed and added to the MLX image,
-*	this function displays the MLX image to the window.
-*/
 void	render(t_fractol *f)
 {
 	int		x;
