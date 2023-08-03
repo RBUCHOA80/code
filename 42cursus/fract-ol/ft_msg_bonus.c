d9ab4d181a6a677719aeeb4c199db6a69767ac5f
RUCHOA <rodrigo.uchoa@hotmail.com>
2023-08-02 23:53:53 -0300

    autogit

--- 42cursus/fract-ol/ft_msg_bonus.c
+++ 42cursus/fract-ol/ft_msg_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 19:38:49 by ruchoa            #+#    #+#             */
-/*   Updated: 2023/08/02 21:03:56 by ruchoa           ###   ########.fr       */
+/*   Updated: 2023/08/02 23:53:34 by ruchoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ void	ft_msg(void)
 {
 	ft_printf("Incorrect fract-ol's set or number of parameters.\n");
 	ft_printf("\tValid options:\n");
-	ft_printf("\t\t./fractol m -> for Mandelbrot fract-ol Set\n");
-	ft_printf("\t\t./fractol b -> for Burning Ship fract-ol Set\n");
-	ft_printf("\t\t./fractol t -> for Tricorn fract-ol Set\n");
-	ft_printf("\t\t./fractol j -> for Julia fract-ol Preset\n");
-	ft_printf("\t\t./fractol j x.x y.y -> for Julia fract-ol Set\n");
+	ft_printf("\t\t./fractol_bonus m -> for Mandelbrot fract-ol Set\n");
+	ft_printf("\t\t./fractol_bonus b -> for Burning Ship fract-ol Set\n");
+	ft_printf("\t\t./fractol_bonus t -> for Tricorn fract-ol Set\n");
+	ft_printf("\t\t./fractol_bonus j -> for Julia fract-ol Preset\n");
+	ft_printf("\t\t./fractol_bonus j x.x y.y -> for Julia fract-ol Set\n");
 	exit(1);
 }
