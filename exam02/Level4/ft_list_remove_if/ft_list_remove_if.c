e8ea2ca8a5cf5043f24f70db0a10531bc79190ea
RUCHOA <rodrigo.uchoa@hotmail.com>
2023-08-11 20:17:38 -0300

    autogit

--- exam02/Level4/ft_list_remove_if/ft_list_remove_if.c
+++ exam02/Level4/ft_list_remove_if/ft_list_remove_if.c
@@ -6,7 +6,7 @@
 /*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 21:27:32 by ruchoa            #+#    #+#             */
-/*   Updated: 2023/08/11 20:16:10 by ruchoa           ###   ########.fr       */
+/*   Updated: 2023/08/11 20:17:34 by ruchoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,20 +62,14 @@ int	main(void)
 	t_list	*nod1;
 	t_list	*nod2;
 	t_list	*nod3;
-	char	*str1;
-	char	*str2;
-	char	*str3;
 
-	str1 = "Put your";
-	str2 = "sample text";
-	str3 = "inside here!";
 	nod1 = malloc(sizeof(t_list));
 	nod2 = malloc(sizeof(t_list));
 	nod3 = malloc(sizeof(t_list));
 	begin_list = &nod1;
-	nod1->data = str1;
-	nod2->data = str2;
-	nod3->data = str3;
+	nod1->data = "Put your";
+	nod2->data = "sample text";
+	nod3->data = "inside here!";
 	nod1->next = nod2;
 	nod2->next = nod3;
 	nod3->next = NULL;
