5c6c1ce62a4cf3420003d53a408a0c5dfd552fb5
RUCHOA <rodrigo.uchoa@hotmail.com>
2023-08-21 23:58:10 -0300

    autogit

--- 42cursus/philo/ft_create_rules.c
+++ 42cursus/philo/ft_create_rules.c
@@ -6,7 +6,7 @@
 /*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 21:31:18 by ruchoa            #+#    #+#             */
-/*   Updated: 2023/08/21 23:42:27 by ruchoa           ###   ########.fr       */
+/*   Updated: 2023/08/21 23:56:41 by ruchoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,7 @@ t_argv	*ft_create_rules(char **argv)
 	rules->tts = ft_atoi(argv[4]);
 	if (argv[5])
 		rules->pme = ft_atoi(argv[5]);
+	else
+		rules->pme = -1;
 	return (rules);
 }
--- 42cursus/philo/philo.c
+++ 42cursus/philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 21:17:24 by ruchoa            #+#    #+#             */
-/*   Updated: 2023/08/21 23:51:45 by ruchoa           ###   ########.fr       */
+/*   Updated: 2023/08/21 23:57:36 by ruchoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	philo(t_argv *rules)
 	printf("time_to_die = %li\n", rules->ttd);
 	printf("time_to_eat = %li\n", rules->tte);
 	printf("time_to_sleep = %li\n", rules->tts);
-	if (rules->pme)
+	if (rules->pme > 0)
 		printf("number_of_times_each_philosopher_must_eat = %i\n", rules->pme);
 	ft_free(rules);
 	return (0);
