7e4b09fad1cfa39f6ba72901ebde8121e065d68f
RUCHOA <rodrigo.uchoa@hotmail.com>
2023-10-01 15:38:51 -0300

    autogit

--- 42cursus/minishell/minishell.c
+++ 42cursus/minishell/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 21:06:46 by ruchoa            #+#    #+#             */
-/*   Updated: 2023/10/01 15:02:42 by ruchoa           ###   ########.fr       */
+/*   Updated: 2023/10/01 15:38:22 by ruchoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	minishell(t_minishell *data)
 	ft_banner();
 	while (1)
 	{
-		printf("%s%s%s%s%s", GREEN, " RUCHOA_@_MINISHELL", WHITE, ":", BLUE);
+		printf("%s%s%s%s%s", GREEN, " RUCHOA_@_MINISHELL", WHITE, ":", RED);
 		line = readline(ft_strjoin(getcwd(buff, PATH_MAX), "\e[0m$ "));
 		line = ft_expand(data, line);
 		//ft_token(data, line);
