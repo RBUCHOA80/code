e985808e99e2bef3ea985b493fe2bdf5c9d644c7
RUCHOA <rodrigo.uchoa@hotmail.com>
2023-10-11 22:46:44 -0300

    autogit

--- 42cursus/minishell/srcs/main/ft_init.c
+++ 42cursus/minishell/srcs/main/ft_init.c
@@ -6,7 +6,7 @@
 /*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 21:22:41 by ruchoa            #+#    #+#             */
-/*   Updated: 2023/10/11 22:04:58 by ruchoa           ###   ########.fr       */
+/*   Updated: 2023/10/11 22:44:26 by ruchoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,26 @@ int	ft_init_env(t_minishell *data, char **arge)
 	return (EXIT_SUCCESS);
 }
 
-void	ft_exit2(int sig)
+void	ft_sigint(int sig)
+{
+	if (sig == 2)
+		printf("\nworking");
+	else
+		printf("\nSIGINT=%i", sig);
+}
+
+void	ft_sigquit(int sig)
 {
 	if (sig == SIGQUIT)
-		printf("(135)%i\n", sig);
+		printf("\nSIGQUIT=%i\n", sig);
 	else
-		printf("(B)%i\n", sig);
+		printf("\nCTRL+C = %i\n", sig);
 }
 
 int	ft_init_signal(void)
 {
-	signal(SIGINT, &ft_exit);
-	signal(SIGQUIT, &ft_exit2);
+	signal(SIGINT, &ft_sigint); //CTRL + C
+	signal(SIGQUIT, &ft_sigquit);
 	return (EXIT_SUCCESS);
 }
 
