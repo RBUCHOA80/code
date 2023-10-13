9488d5ba4cb9f2bb0b32edcc56671b4e82a10763
RUCHOA <rodrigo.uchoa@hotmail.com>
2023-10-12 21:27:45 -0300

    autogit

--- 42cursus/minishell/srcs/tools/ft_strcmp.c
+++ 42cursus/minishell/srcs/tools/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 19:31:37 by ruchoa            #+#    #+#             */
-/*   Updated: 2023/10/12 19:53:53 by ruchoa           ###   ########.fr       */
+/*   Updated: 2023/10/12 21:24:27 by ruchoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,14 @@ int	ft_strcmp(char *s1, char *s2)
 	len_s1 = ft_strlen(s1);
 	len_s2 = ft_strlen(s2);
 	if (len_s1 < len_s2)
-		return (ft_strncmp(s1, s2, len_s1));
+	{
+		if (ft_strncmp(s1, s2, len_s1))
+			return (RETURN_FAILURE);
+	}
 	else
-		return (ft_strncmp(s1, s2, len_s2));
+	{
+		if (ft_strncmp(s1, s2, len_s2))
+			return (RETURN_FAILURE);
+	}
+	return (RETURN_SUCCESS);
 }
