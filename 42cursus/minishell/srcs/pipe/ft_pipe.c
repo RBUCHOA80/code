4b33310ece4b186ad15fe469573b22b95de0701a
RUCHOA <rodrigo.uchoa@hotmail.com>
2023-11-06 00:07:01 -0300

    autogit

--- 42cursus/minishell/srcs/pipe/ft_pipe.c
+++ 42cursus/minishell/srcs/pipe/ft_pipe.c
@@ -6,7 +6,7 @@
 /*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 22:47:31 by ruchoa            #+#    #+#             */
-/*   Updated: 2023/11/05 19:31:01 by ruchoa           ###   ########.fr       */
+/*   Updated: 2023/11/06 00:04:52 by ruchoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ int	ft_prepare_pipe(t_minishell *data)
 	fd_out = 6;
 	fd_in = fd_in + ((data->pipe_count - ft_count_pipe(data) - 1) * 2);
 	fd_out = fd_out + ((data->pipe_count - ft_count_pipe(data)) * 2);
-	printf("fd_in=%i\n", fd_in);
-	printf("fd_out=%i\n", fd_out);
 	dup2(fd_in, STDIN_FILENO);
 	dup2(fd_out, STDOUT_FILENO);
 	close(fd_in);
