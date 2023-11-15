fbb72b72fe9290d4eae91bb2b58c09623d58aaec
RUCHOA <rodrigo.uchoa@hotmail.com>
2023-11-15 13:46:28 -0300

    autogit

--- 42cursus/minishell/srcs/builtin/ft_export.c
+++ 42cursus/minishell/srcs/builtin/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: ruchoa <ruchoa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 13:04:02 by ruchoa            #+#    #+#             */
-/*   Updated: 2023/11/05 19:31:01 by ruchoa           ###   ########.fr       */
+/*   Updated: 2023/11/15 13:45:20 by ruchoa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,13 @@ int	ft_export(t_minishell *data)
 {
 	char	*cmd;
 
+	if (data->token->next == NULL)
+		return (EXIT_SUCCESS);
 	data->token = data->token->next;
-	if (ft_isalpha(*data->token->content) == EXIT_FAILURE && \
+
+	char **strs = ft_split(data->token->content, '=');
+
+	if (ft_isalpha(*data->token->content) == EXIT_SUCCESS && \
 		*data->token->content != '_')
 	{
 		ft_fprintf(STDOUT_FILENO, \
@@ -27,6 +32,11 @@ int	ft_export(t_minishell *data)
 	}
 	else if (ft_strchr(data->token->content, '=') == NULL)
 		return (EXIT_SUCCESS);
+	else if (ft_search_env(data->env, strs[0]))
+	{
+		ft_fprintf(2, "entrou\n");
+		return (EXIT_SUCCESS);
+	}
 	else
 	{
 		cmd = data->token->content;
